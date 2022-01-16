6edd7a4333d5fb53dc08ae6be8fce9dcb6600332
PiyushGG18 <gawadepiyush007@gmail.com>
2022-01-16 16:02:43 +0530

    Commit Message changed successfully

--- PiyushGG18.py
+++ PiyushGG18.py
@@ -1,4 +1,3 @@
 Friends is favorite TV show
 It is funny and basically revolves around concept of friendship
-Mckenna Grace
-Yes
+I think I learned alot in this workshop and new things which i dont know when i might learn was taught here in a wonderful manner
